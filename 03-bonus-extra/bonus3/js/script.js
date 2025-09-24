754011a4b639162a55dde0fd87b00818421092f9
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-09-24 16:33:17 +0200

    feat: 03-bonus-extra Completed

--- 03-bonus-extra/bonus3/js/script.js
+++ 03-bonus-extra/bonus3/js/script.js
@@ -1,2 +1,21 @@
 // Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 
+/**
+ * 
+ */
+
+const userNumberString = prompt("Inserisci un numero di 4 cifre");
+console.log(userNumberString);
+if (!userNumberString) {
+    console.error("Input non valido. Ricaricare la pagina");
+} else {
+    if (userNumberString.length === 4) {
+        let somma = 0;
+        for (let i = 0; i < userNumberString.length; i++) {
+            somma += parseInt(userNumberString[i]);
+        }
+        console.log("La somma delel cifre del tuo numero è:", somma);
+    } else {
+        console.error("L'unghezza dell'input errata. Ricaricare la pagina");
+    }
+}
