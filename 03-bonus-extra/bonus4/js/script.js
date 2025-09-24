3a0048a9ec6b65fe681a824b864ad65763eb9f8d
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-09-24 16:40:56 +0200

    feat: 04-bonus-extra Completed

--- 03-bonus-extra/bonus4/js/script.js
+++ 03-bonus-extra/bonus4/js/script.js
@@ -1,1 +1,13 @@
-// 
+// Calcola la somma e la media dei primi 10 numeri.
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let sum = 0;
+let average = 0;
+
+for (let i = 0; i < numbersArray.length; i++) {
+    const curNumber = numbersArray[i];
+    sum += curNumber;
+}
+average = sum / numbersArray.length;
+
+console.log(`La media e la somma di [${numbersArray}] sono rispettivamente: ${sum} e ${average} !`)
